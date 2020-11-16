25a804c1024dbae8ee41c31f73579088c162a36a
Temo <temur.chichua@iliauni.edu.ge>
2020-11-16 20:49:17 +0400

    from PC

--- demo.py
+++ demo.py
@@ -5,3 +5,20 @@ onge = page_setup()
 url = onge.get_url(category=0, page=1)
 print(url)
 # https://on.ge/category/პოლიტიკა?page=150
+title = response.css('title::text').getall()
+
+
+
+pages = response.css("a.overlay-link.link.js-link")
+for page in pages:
+     title = page.css("a::text").get()
+     url = page.css("a::attr(href)").get()
+     link = dict(title=title, url=url)
+     print(link)
+
+pages = response.css("a.overlay-link.link.js-link")
+
+page = pages_on_page[index]
+
+title = page.css("a::text").get()
+link = page.css("a::attr(href)").get()
